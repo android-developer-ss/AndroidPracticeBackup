90b9468f561ac1400dacfcdbdc4e60823f974900
Snehal Sutar <snehal.v.sutar@gmail.com>
2015-10-27 13:45:06 -0500

    initial commit

---
+++ app/src/main/java/com/example/snehal/allbuttonssample/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.snehal.allbuttonssample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ToggleButton toggleButton1, toggleButton2;
+    private Button btnDisplay,nextscreen;
+    EditText eText;
+    Button btn;
+    public final static String EXTRA_MESSAGE = "com.example.snehal.allbuttonssample.MESSAGE";
+    //**********************************************************************************************
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //------------------------------------------------------------------------------------------
+        addListenerOnButton();
+
+        //------------------------------------------------------------------------------------------
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    //**********************************************************************************************
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    //**********************************************************************************************
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //**********************************************************************************************
+    public void addListenerOnButton() {
+        toggleButton1 = (ToggleButton) findViewById(R.id.toggleButton1);
+        toggleButton2 = (ToggleButton) findViewById(R.id.toggleButton2);
+        btnDisplay = (Button) findViewById(R.id.btnDisplay);
+
+        btnDisplay.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                StringBuffer result = new StringBuffer();
+                result.append("toggleButton1 : ").append(toggleButton1.getText());
+                result.append("\ntoggleButton2 : ").append(toggleButton2.getText());
+
+                Toast.makeText(MainActivity.this, result.toString(),
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+
+        //------------------------------------------------------------------------------------------
+        eText = (EditText) findViewById(R.id.edittext);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String str = eText.getText().toString();
+                Toast msg = Toast.makeText(getBaseContext(),str,Toast.LENGTH_LONG);
+                msg.show();
+            }
+        });
+
+    }
+
+    //**********************************************************************************************
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        EditText editText = (EditText) findViewById(R.id.edittext);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
